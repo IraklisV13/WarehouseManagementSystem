5e81be4d86fa463d3dd5f21baa6f28cf3846b6c9
Iraklis <iraklisves@gmail.com>
2024-05-24 13:36:02 +0000

    context changes

--- Data/DBContext/WarehouseManagementSystemDBContext.cs
+++ Data/DBContext/WarehouseManagementSystemDBContext.cs
@@ -5,13 +5,12 @@ namespace Data.DBContext
 {
     public class WarehouseManagementSystemDBContext : DbContext
     {
-
         public DbSet<Warehouse> Warehouses { get; set; }
         public DbSet<InventoryItemCategory> InventoryItemCategories { get; set; }
         public DbSet<InventoryItem> InventoryItems { get; set; }
         public DbSet<InventoryTransaction> InventoryTransactions { get; set; }
 
-        public WarehouseManagementSystemDBContext() : base("name=WarehouseManagementSystemDBContextConnectionString")
+        public WarehouseManagementSystemDBContext() : base("name=WarehouseManagementDatabase")
         {
         }
 
@@ -22,23 +21,26 @@ namespace Data.DBContext
             modelBuilder.Entity<Warehouse>()
                 .HasMany(w => w.InventoryItems)
                 .WithRequired(i => i.Warehouse)
-                .HasForeignKey(i => i.WarehouseId);
+                .HasForeignKey(i => i.WarehouseId)
+                .WillCascadeOnDelete(false);
 
             modelBuilder.Entity<InventoryItemCategory>()
                 .HasMany(c => c.InventoryItems)
                 .WithRequired(i => i.Category)
-                .HasForeignKey(i => i.CategoryId);
+                .HasForeignKey(i => i.CategoryId)
+                .WillCascadeOnDelete(false);
 
             modelBuilder.Entity<InventoryItem>()
                 .HasMany(i => i.InventoryTransactions)
                 .WithRequired(t => t.InventoryItem)
-                .HasForeignKey(t => t.InventoryItemId);
+                .HasForeignKey(t => t.InventoryItemId)
+                .WillCascadeOnDelete(false);
 
             modelBuilder.Entity<Warehouse>()
                 .HasMany(w => w.InventoryTransactions)
                 .WithRequired(t => t.Warehouse)
-                .HasForeignKey(t => t.WarehouseId);
+                .HasForeignKey(t => t.WarehouseId)
+                .WillCascadeOnDelete(false);
         }
-
     }
 }
